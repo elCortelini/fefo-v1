11b74242ddb0fd73aafc0f39d314234e53c5acc9
elCortelini <elcortelini@gmail.com>
2026-08-15 18:47:04 -0300

    app v047: lancamento limpo da versao v047 para invalidação de cache

--- app_android/lib/pages/tela_sobre.dart
+++ app_android/lib/pages/tela_sobre.dart
@@ -64,7 +64,7 @@ class TelaSobre extends StatelessWidget {
                           border: Border.all(color: const Color(0xFFDC4900), width: 1.5),
                         ),
                         child: const Text(
-                          'Versão do App: FEFO App v046 (1.0.46+46)',
+                          'Versão do App: FEFO App v047 (1.0.47+47)',
                           style: TextStyle(
                             fontFamily: 'KGPen',
                             fontSize: 16,
