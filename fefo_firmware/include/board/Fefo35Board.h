1c43dad7a0688071a371d2b53169f14e9483e57b
elCortelini <elcortelini@gmail.com>
2026-08-19 16:40:18 -0300

    Tune firmware audio clarity in v1.080

--- fefo_firmware/include/board/Fefo35Board.h
+++ fefo_firmware/include/board/Fefo35Board.h
@@ -7,8 +7,8 @@
 namespace fefo::board {
 
 inline constexpr char kBoardName[] = "FEFO-35-V0";
-inline constexpr char kBleName[] = "FEFO_BLE_V1079";
-inline constexpr char kFirmwareVersion[] = "1.079";
+inline constexpr char kBleName[] = "FEFO_BLE_V1080";
+inline constexpr char kFirmwareVersion[] = "1.080";
 inline constexpr char kProtocolVersion[] = "0.1";
 
 // TFT SPI ILI9488 480x320 validada no protótipo com a configuração funcional
