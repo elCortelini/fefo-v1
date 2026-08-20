5123fe607afe99dcae04d9f40ab5af491d893440
elCortelini <elcortelini@gmail.com>
2026-08-20 13:45:16 -0300

    fix black screen after bluetooth connection

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.075';
-const String fefoAppVersionName = '1.075';
-const int fefoAppBuildNumber = 1075;
+const String fefoAppVersionLabel = 'v1.076';
+const String fefoAppVersionName = '1.076';
+const int fefoAppBuildNumber = 1076;
