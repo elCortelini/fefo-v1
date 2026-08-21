c25e404cdbb2bc9ce764a7a668c8784a8470c46e
elCortelini <elcortelini@gmail.com>
2026-08-21 09:44:05 -0300

    hide alarms from main menu

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.081';
-const String fefoAppVersionName = '1.081';
-const int fefoAppBuildNumber = 1081;
+const String fefoAppVersionLabel = 'v1.082';
+const String fefoAppVersionName = '1.082';
+const int fefoAppBuildNumber = 1082;
