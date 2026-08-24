20db27e5d1ce503ac9b494d397f1e0d4fa2a7b04
elCortelini <elcortelini@gmail.com>
2026-08-24 17:13:45 -0300

    fix(app): corrige deteccao de atualizacao 1.101

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.100';
-const String fefoAppVersionName = '1.100';
-const int fefoAppBuildNumber = 1098;
+const String fefoAppVersionLabel = 'v1.101';
+const String fefoAppVersionName = '1.101';
+const int fefoAppBuildNumber = 1101;
