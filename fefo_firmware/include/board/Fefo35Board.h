13e600adfa7a29e3781e55d47e56755a9d481c0d
elCortelini <elcortelini@gmail.com>
2026-08-24 16:53:51 -0300

    fix(sync): preserva catalogo e exibe player no menu

--- fefo_firmware/include/board/Fefo35Board.h
+++ fefo_firmware/include/board/Fefo35Board.h
@@ -7,8 +7,8 @@
 namespace fefo::board {
 
 inline constexpr char kBoardName[] = "FEFO-35-V0";
-inline constexpr char kBleName[] = "FEFO_BLE_V1086";
-inline constexpr char kFirmwareVersion[] = "1.086";
+inline constexpr char kBleName[] = "FEFO_BLE_V1087";
+inline constexpr char kFirmwareVersion[] = "1.087";
 inline constexpr char kProtocolVersion[] = "0.1";
 
 // TFT SPI ILI9488 480x320 validada no protótipo com a configuração funcional
