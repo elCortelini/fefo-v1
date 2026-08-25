1dd37ac6f8d8f89db889892b5c071ef9a2b28747
elCortelini <elcortelini@gmail.com>
2026-08-25 11:57:03 -0300

    fix(app): restore working wifi network request

--- fefo_app/android/app/src/main/kotlin/com/elcortelini/fefo/app/MainActivity.kt
+++ fefo_app/android/app/src/main/kotlin/com/elcortelini/fefo/app/MainActivity.kt
@@ -149,8 +149,6 @@ class MainActivity : FlutterActivity() {
             .setWpa2Passphrase(password).build()
         val request = NetworkRequest.Builder()
             .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED)
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_TRUSTED)
             .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .setNetworkSpecifier(specifier).build()
         var completed = false
@@ -165,7 +163,7 @@ class MainActivity : FlutterActivity() {
                 } }
             }
         }
-        manager.requestNetwork(request, networkCallback!!, 60000)
+        manager.requestNetwork(request, networkCallback!!, 30000)
     }
 
     private fun disconnect() {
