53761842484461b87cbea58b39fe46f9e35540f9
elCortelini <elcortelini@gmail.com>
2026-08-25 10:03:40 -0300

    fix(app): corrige wifi e aproveita area inferior

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.101';
-const String fefoAppVersionName = '1.101';
-const int fefoAppBuildNumber = 1101;
+const String fefoAppVersionLabel = 'v1.102';
+const String fefoAppVersionName = '1.102';
+const int fefoAppBuildNumber = 1102;
