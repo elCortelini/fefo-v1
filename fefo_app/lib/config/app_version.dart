db6bfbe575942cb694945bed805fdc36c21c9f66
elCortelini <elcortelini@gmail.com>
2026-08-25 11:57:03 -0300

    fix(app): restore working wifi network request

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.108';
-const String fefoAppVersionName = '1.108';
-const int fefoAppBuildNumber = 1108;
+const String fefoAppVersionLabel = 'v1.109';
+const String fefoAppVersionName = '1.109';
+const int fefoAppBuildNumber = 1109;
