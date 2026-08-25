ab3dddc8ec04b410793a2bc908733f491331e3df
elCortelini <elcortelini@gmail.com>
2026-08-25 12:23:57 -0300

    fix(sync): flush wifi responses before transfer reboot

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.109';
-const String fefoAppVersionName = '1.109';
-const int fefoAppBuildNumber = 1109;
+const String fefoAppVersionLabel = 'v1.110';
+const String fefoAppVersionName = '1.110';
+const int fefoAppBuildNumber = 1110;
