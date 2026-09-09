4186b22ef934854bf0fb12dc9ad05db9e9535902
elCortelini <elcortelini@gmail.com>
2026-09-09 09:05:49 -0300

    FEFO v1.123: validar assinatura canonica do catalogo

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.121';
-const String fefoAppVersionName = '1.121';
-const int fefoAppBuildNumber = 1121;
+const String fefoAppVersionLabel = 'v1.123';
+const String fefoAppVersionName = '1.123';
+const int fefoAppBuildNumber = 1123;
