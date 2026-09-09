b17fb8a52c265a1b0c27df7e8172239c750de5ca
elCortelini <elcortelini@gmail.com>
2026-09-09 08:34:31 -0300

    FEFO v1.120: corrigir cache da assinatura do catalogo

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.119';
-const String fefoAppVersionName = '1.119';
-const int fefoAppBuildNumber = 1119;
+const String fefoAppVersionLabel = 'v1.120';
+const String fefoAppVersionName = '1.120';
+const int fefoAppBuildNumber = 1120;
