4c5f8b58a947a68eed1e4761e580c55dd2c553c8
elCortelini <elcortelini@gmail.com>
2026-09-10 09:23:17 -0300

    FEFO v1.126: corrigir identificacao interna do app

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.125';
-const String fefoAppVersionName = '1.125';
-const int fefoAppBuildNumber = 1125;
+const String fefoAppVersionLabel = 'v1.126';
+const String fefoAppVersionName = '1.126';
+const int fefoAppBuildNumber = 1126;
