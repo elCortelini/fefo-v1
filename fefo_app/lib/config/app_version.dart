6bf3d4f1c1f964163a9f384b2fedc93a4548c1c5
elCortelini <elcortelini@gmail.com>
2026-09-10 13:19:05 -0300

    catalog: manter sons por evento sem ativar alarme

--- fefo_app/lib/config/app_version.dart
+++ fefo_app/lib/config/app_version.dart
@@ -1,4 +1,4 @@
 /// Identidade da versão apresentada ao usuário e usada nos metadados do app.
-const String fefoAppVersionLabel = 'v1.128';
-const String fefoAppVersionName = '1.128';
-const int fefoAppBuildNumber = 1128;
+const String fefoAppVersionLabel = 'v1.126';
+const String fefoAppVersionName = '1.126';
+const int fefoAppBuildNumber = 1126;
